e6b22ef876b64f1eefb5cc61ad6e2158537929b3
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-12 13:16:07 -0500

    finish take a number exercise

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 // CHICKEN MONKEY
 
-for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-  // Divide the current number by 2, and check if the remainder is 0
-  if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
-      console.log("ChickenMonkey")
-  } else if (currentNumber % 5 === 0) {
-      console.log("Chicken")
-  } else if (currentNumber % 7 === 0) {
-      console.log("Monkey")
-  } else {
-    console.log(currentNumber)
-  }
-} 
+// for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+//   // Divide the current number by 2, and check if the remainder is 0
+//   if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+//       console.log("ChickenMonkey")
+//   } else if (currentNumber % 5 === 0) {
+//       console.log("Chicken")
+//   } else if (currentNumber % 7 === 0) {
+//       console.log("Monkey")
+//   } else {
+//     console.log(currentNumber)
+//   }
+// } 
 
 
 
@@ -20,3 +20,16 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
 // A local nightclub is having a Battle of the Bands night in a few months. They expect many bands to sign up because the grand prize is $20,000. To make things easier on the nightclub management, they want each band to be assigned a number so that they can easily keep track of the order in which the bands will perform.
 
 // Your job is to write a function that accepts any band name as an argument. The function will increment a global variable by one each time it is invoked, and return that number, and the band name concatenated together.
+
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    bandNumber += 1;
+    return `${bandNumber}. ${bandName}`;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
